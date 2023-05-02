8d7e7a7de5b4aaeafc369628c6dbcca45a08905b
BransTiong <brans98.dev@gmail.com>
2023-05-03 02:27:08 +0800

    Added the ChatbotPrompt on constants. And also book data file

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,9 +1,15 @@
+import { ChatGPTMessage } from "@/lib/openai-stream"
 import { MessageArraySchema } from "@/lib/validators/message"
 
 export async function POST(req: Request){
     const {messages} = await req.json()
 
     const parsedMessages = MessageArraySchema.parse(messages)
+
+    const outboundMessages: ChatGPTMessage [] = parsedMessages.map ((message) => ({
+        role: message.isUserMessage ? 'user' : 'system',
+        content: message.text,
+    }))
 
 
     console.log('Endpoint works')
